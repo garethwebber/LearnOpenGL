fe3403968422bee62f8641c998103932a634513d
Gareth Webber <github@garethwebber.com>
2024-11-03 20:33:15 +0000

    Multicolored triangle.

--- src/main.c
+++ src/main.c
@@ -5,17 +5,21 @@
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+	"out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+	"   ourColor = aColor;\n"
     "}\0";
 
 
 const char *fragShaderSource = "#version 330 core\n"
+	"in vec3 ourColor;\n"
 	"out vec4 FragColor;\n"
 	"void main()\n"
 	"{\n"
-	"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"     FragColor = vec4(ourColor, 1.0);\n"
 	"}\0"; 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -70,15 +74,22 @@ int main()
 	glGenVertexArrays(1, &VAO);  
 	glGenBuffers(1, &VBO); 
 	float vertices[] = {
-    	-0.5f, -0.5f, 0.0f,
-     	0.5f, -0.5f, 0.0f,
-     	0.0f,  0.5f, 0.0f
+    	// positions         // colors
+     	0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+    	-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+     	0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
 	}; 		
     glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);   
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+	glEnableVertexAttribArray(1);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
     glBindVertexArray(0); 
 
